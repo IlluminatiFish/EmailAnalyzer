e41479617ba5aa1351ba0466e9c75bb43c58121e
IlluminatiFish <45714340+IlluminatiFish@users.noreply.github.com>
2020-09-07 03:32:40 +0100

    Updated EmailHeaderExtractor script source
    This update brings along, a new domainExtraction function that is more efficient now than the previous version. And a little adjustment to the output of the data, where the List of IPs has been named Email Traceroute to more accurately represent what it outputs, changed the way the origin_ip variable gets used, as it use to output None when in actual fact the last IP on the traceroute is usually the source. Also added an exception incase ip-api.com can't handle the API request
    
    Bug Fixes:
    - Fixed a bug with the domainExtraction function

--- EmailHeaderExtractor.py
+++ EmailHeaderExtractor.py
@@ -1,22 +1,24 @@
-import re, requests, ipaddress
+import re
+import requests
+import ipaddress
 
 def validateIP(ip):
     try:
-        ipaddress.ip_address(ip)
-        return True
+        i = ipaddress.ip_address(ip)
+        if i.is_global:
+            return True
+        return False
     except ValueError:
         return False
 
-def reformatDomains(list_subdomains, size):     
-    starting_index = len(list_subdomains) - size
-    split_domain = list_subdomains[starting_index:]
-    domain = ""
-    for i in range(len(split_domain)):
-        if i == len(split_domain) - 1:
-            domain += split_domain[i]
-            break
-        domain += split_domain[i]+"."
-    return domain
+def domainExtraction(domain):
+    if len(domain.split('.')) == 2: #Domain only has one dot meaning its the actual domain
+        return domain #Return the domain given
+    elif len(domain.split('.')) > 2:
+        o = domain.split('.', 1) #Split at the first dot seen
+        output = o[1]
+        return output #Output the domain without the front bit
+        
 
 def getInformation(datatype, data):
     if data is None:
@@ -24,8 +26,10 @@ def getInformation(datatype, data):
     else:
         url = "http://ip-api.com/json/{}".format(data)
         req = requests.get(url)
-        reqjson = req.json()
-        
+        try:
+            reqjson = req.json()
+        except json.decoder.JSONDecodeError as err:
+            print('JSON Error, unable to gather info on ip/domain')
         if str(datatype) == "ISP":
             try:
                 isp = reqjson['isp']
@@ -34,7 +38,6 @@ def getInformation(datatype, data):
             except KeyError:
                 isp = None
             return isp
-            
         if str(datatype) == "ASN":
             try:
                 asn = reqjson['as']
@@ -43,7 +46,6 @@ def getInformation(datatype, data):
             except KeyError:
                 asn = None
             return asn
-            
         if str(datatype) == "ORG":
             try:
                 org = reqjson['org']
@@ -56,6 +58,7 @@ def getInformation(datatype, data):
 content = []
 sources = []
 response = {}
+ips = []
 input("Enter email headers:\n")
 while True:
     line = input()
@@ -64,22 +67,36 @@ while True:
     else:
         break
 
+
 emailer = None
 source_mail_server = None
 mailer = None
 origin_ip = None
 reply = None
 
+
 for x in content:
-    prefix = str(x.split(": ")[0]) 
+    prefix = str(x.split(": ")[0])
     if 'Received' in x:
         if prefix == "Received":
             if 'Received: from' in x:
+
+                #print(x)
+                ip_pattern = re.compile(r'(?:^|\b(?<!\.))(?:1?\d\d?|2[0-4]\d|25[0-5])(?:\.(?:1?\d\d?|2[0-4]\d|25[0-5])){3}(?=$|[^\w.])') # need to strengthen the regex here
+                ip = re.findall(ip_pattern, x)
+                
+                for i in range(len(ip)):
+                    #print(ip[i])
+                    if validateIP(ip[i]):
+                        ips.append(ip[i])
+            
+                #print(m)
                 if 'prod.protection.outlook.com' in x: #Patch for Outlook emails as they start with Outlook email protection domains instead of the actual domain that the email came from like seen on Gmail
                     continue
-
+                
                 source = x.replace('Received: from ', '').split(" ")[0]
                 sources.append(source)
+                
 
     if 'From' in x:
         if prefix == "From":
@@ -109,8 +126,10 @@ for x in content:
     if 'Return-Path' in x:
         if prefix == "Return-Path":
             data = x.split(": ")[1]
-            reply = data.replace('<', '').replace('>', '')       
-            
+            reply = data.replace('<', '').replace('>', '')      
+
+    
+
 print("")
 print("=== Extraction Information ===")
 
@@ -124,14 +143,32 @@ print(" - ORG:",getInformation("ORG", source_mail_server))
 
 print("[~] Mailer:",mailer)
 
+print("[~] Return Email:",reply)
+
+z = 1
+x = len(ips)
+print("[~] Email Traceroute [{} hop(s)]:".format(len(ips)))
+
+for ip in ips:
+    if z == 1:
+        print(" [SOURCE MAIL SERVER] "+str(z)+":",ip,"- ("+str(getInformation("ASN", ip)),"/",str(getInformation("ISP", ip)),"/",str(getInformation("ORG", ip))+")")
+    else:
+        #print(x, "=", z)
+        if x == z:
+            if origin_ip is None:
+                origin_ip = ips[x-1]
+                print(" [SOURCE USER IP] "+str(z)+":",ip,"- ("+str(getInformation("ASN", ip)),"/",str(getInformation("ISP", ip)),"/",str(getInformation("ORG", ip))+")")
+        else:
+            print(" [-] "+str(z)+":",ip,"- ("+str(getInformation("ASN", ip)),"/",str(getInformation("ISP", ip)),"/",str(getInformation("ORG", ip))+")")
+    z += 1
+
+    
 print("[~] Source IP:",origin_ip)
 print("  - ASN:",getInformation("ASN", origin_ip))
 print("  - ISP:",getInformation("ISP", origin_ip))
 print("  - ORG:",getInformation("ORG", origin_ip))
 
-print("[~] Return Email:",reply)
-
-print("[~] Notes:")
+print("[~] Notes:")    
 
 source_email_domain = emailer.split('@')[1]
 print(" - Source Email Domain =",source_email_domain)
@@ -142,8 +179,8 @@ print("  - ORG:",getInformation("ORG", source_email_domain))
 size = len(source_mail_server.split(".")) - 1 #Indexes of lists start at 0 so we must subtract 1
 
 if(validateIP(source_mail_server) == False):
-    email_server_domain = reformatDomains(source_mail_server.split("."), size)
-    print(" - Source Email Server Domain =",email_server_domain)
+    email_server_domain = domainExtraction(source_mail_server)
+    print(" - Source Email Server Domain =",email_server_domain) 
 
     print("  - ASN:",getInformation("ASN", email_server_domain))
     print("  - ISP:",getInformation("ISP", email_server_domain))
